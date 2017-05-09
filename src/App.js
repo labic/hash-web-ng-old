ea8bb5834826bed1629874a6908eae4b890d2bef
Bianca Gonçalves <bih_goncalves@hotmail.com>
2017-05-09 14:16:52 -0300

    Arrumando navbar com nome

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Colors from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: Colors.white,
+    primary1Color: '#545454',
+    fontSize: "15px",
+  },
+  appBar: {
+    height: 40,
+  },
+});
 const App = () => (
-  <MuiThemeProvider>
-    <AppBar
-      title="Hash Web"
-      iconClassNameRight="muidocs-icon-navigation-expand-more"
-     />
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <AppBar title="Clipper" showMenuIconButton={false} style={{textAlign: "center"}}>
+      </AppBar>
   </MuiThemeProvider>
 );
 
 export default App;
+
