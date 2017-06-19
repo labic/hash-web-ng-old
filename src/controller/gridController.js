ec6b14e40ba18a1b9a3f01a5052ad42ad1e43d1d
Bianca Gonçalves <bih_goncalves@hotmail.com>
2017-06-19 17:30:05 -0300

    acessando dados API

---
+++ src/controller/gridController.js
@@ -0,0 +1,16 @@
+angular.module('angularApp')
+    .controller("gridController", function($scope, $http) { 
+
+    var url = 'https://inep-hash-data-api-dev.herokuapp.com/articles';
+
+    $http({
+        url: url,
+        method:'GET'
+    })
+    .then(function (response) {
+        $scope.novidades = response.data.data; 
+    },
+    function (err) {
+        console.log(err);
+    });
+});
